768c6578eeb822c0098f11a974b5c737449455de
manaspatil7 <121172076+manaspatil7@users.noreply.github.com>
2025-07-23 01:17:13 +0530

    final

--- app.py
+++ app.py
@@ -194,7 +194,4 @@ def index():
     return render_template('upload.html')
 
 if __name__ == '__main__':
-    import os
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
-
+    app.run(debug=True, port=5000)
